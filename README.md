b8a49e83780185e2912c8b6384a66d81ac214c14
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-03-31 17:31:59 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Smart Backwards Kill Word Widget
+
+gif
+
+# Installation
+
+## zgen
+
+
+### Setup plugin
+
+```zsh
+zgen load seletskiy/zsh-smart-backward-kill-word
+```
+
+### .zshrc
+
+```zsh
+bindkey '^W' smart-backward-kill-word
+```
---
+++ plugin.zsh
@@ -0,0 +1,40 @@
+autoload -U match-words-by-style
+
+function smart-backward-kill-word() {
+    local precise
+    zstyle -g precise ':zle:smart-backward-kill-word' precise
+
+    local from_cursor_to_left=(${(z)LBUFFER})
+
+    local number_of_words_left=${#from_cursor_to_left}
+
+    local word_part_before_cursor="${from_cursor_to_left[-1]}"
+    local word_under_cursor=${${(z)BUFFER}[$number_of_words_left]}
+
+    local inside_shell_word=$(((
+        ${#word_under_cursor} > ${#word_part_before_cursor}
+    )))
+
+    local match_style="shell"
+    if [ "$inside_shell_word" = "1" -o "$precise" = "always" ]; then
+        if [[ "${word_part_before_cursor[1]}" =~ "['(]" ]]; then
+            if (( ${#${word_part_before_cursor// /}} > 1 )); then
+                match_style="normal"
+            fi
+        fi
+    fi
+
+    case "$match_style"; in
+        normal)
+            match-words-by-style -w normal -C "['\"([]"
+            ;;
+
+        shell)
+            match-words-by-style -w shell-subword -r '/'
+            ;;
+    esac
+
+    LBUFFER=${matched_words[1]}
+}
+
+zle -N smart-backward-kill-word
